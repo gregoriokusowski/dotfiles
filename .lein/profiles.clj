d1a5f9b611db5beeef27f9147a54d202ad1eca58
Gregório Kusowski <gregorio.kusowski@gmail.com>
2017-10-31 22:59:30 +0100

    Updates

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user  {:plugins  [[jonase/eastwood "0.2.4"]
                     [lein-cljfmt "0.5.7"]
                     [cider/cider-nrepl "0.15.1"]
+                    [refactor-nrepl "2.3.1"]
                     [venantius/yagni "0.1.4"]
                     [venantius/ultra "0.5.1"]]}
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
