47225f4e941ee0d002bfff9eb431ba3369416036
Gregório Kusowski <gregorio.kusowski@gmail.com>
2018-02-21 16:03:36 +0100

    sync

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
-{:user  {:plugins  [[jonase/eastwood "0.2.4"]
+{:user  {:plugins  [[jonase/eastwood "0.2.5"]
                     [lein-cljfmt "0.5.7"]
-                    [cider/cider-nrepl "0.15.1"]
+                    [cider/cider-nrepl "0.16.0"]
                     [refactor-nrepl "2.3.1"]
                     [venantius/yagni "0.1.4"]
-                    [venantius/ultra "0.5.1"]
-                    [lein-ancient "0.6.14"]]}
+                    [venantius/ultra "0.5.2"]
+                    [lein-ancient "0.6.15"]]}
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [org.clojure/tools.namespace "0.3.0-alpha4"]]}
