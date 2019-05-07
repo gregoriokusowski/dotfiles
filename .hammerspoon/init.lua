6ab6914a0c579b35cf4394ac78fa092263a9b698
Gregório Kusowski <gregorio.kusowski@gmail.com>
2019-05-07 07:39:41 +0200

    Support trackball scroll

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,29 @@
+-- https://github.com/tekezo/Karabiner/issues/814#issuecomment-290100483
+-- HANDLE SCROLLING
+local oldmousepos = {}
+-- positive multiplier (== natural scrolling) makes mouse work like traditional scrollwheel
+local scrollmult = 4 
+
+-- The were all events logged, when using `{"all"}`
+mousetap = hs.eventtap.new({0,3,5,14,25,26,27}, function(e)
+	oldmousepos = hs.mouse.getAbsolutePosition()
+	local mods = hs.eventtap.checkKeyboardModifiers()
+    local pressedMouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
+
+    -- If OSX button 4 is pressed, allow scrolling
+    local shouldScroll = 3 == pressedMouseButton
+    if shouldScroll then
+	local dx = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaX'])
+	local dy = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaY'])
+	local scroll = hs.eventtap.event.newScrollEvent({dx * scrollmult, dy * scrollmult},{},'pixel')
+	scroll:post()
+	
+	-- put the mouse back
+	hs.mouse.setAbsolutePosition(oldmousepos)
+	
+	return true, {scroll}
+    else
+	return false, {}
+    end
+end)
+mousetap:start()
