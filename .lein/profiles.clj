b7c379da66967115189187b6576f8389a60b787c
Gregório Kusowski <gregorio.kusowski@gmail.com>
2019-05-29 07:12:13 +0200

    Improve startup time :( of repl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,12 @@
-{:user  {:plugins  [[jonase/eastwood "0.2.5"]
-                    [lein-cljfmt "0.5.7"]
-                    [cider/cider-nrepl "0.16.0"]
-                    [refactor-nrepl "2.3.1"]
-                    [venantius/yagni "0.1.4"]
-                    [venantius/ultra "0.5.2"]
-                    [lein-ancient "0.6.15"]]}
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                       [org.clojure/tools.namespace "0.3.0-alpha4"]]}
+{:user  
+	{:jvm-opts ^:replace ["-Xmx3G"]
+;;        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+;;                       [org.clojure/tools.namespace "0.3.0-alpha4"]]
+;;	:plugins  [[jonase/eastwood "0.2.5"]
+;;		   [lein-cljfmt "0.5.7"]
+;;		   [cider/cider-nrepl "0.16.0"]
+;;		   [refactor-nrepl "2.3.1"]
+;;		   [venantius/yagni "0.1.4"]
+;;		   [venantius/ultra "0.5.2"]
+;;		   [lein-ancient "0.6.15"]]
+}}
