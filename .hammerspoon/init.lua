298f3e53f71955908bb806fc637aec1ff80630da
Gregorio Kusowski <gregorio.kusowski@shopify.com>
2020-08-19 14:21:59 +0200

    Format trackball tweak

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -13,17 +13,17 @@ mousetap = hs.eventtap.new({0,3,5,14,25,26,27}, function(e)
     -- If OSX button 4 is pressed, allow scrolling
     local shouldScroll = 3 == pressedMouseButton
     if shouldScroll then
-	local dx = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaX'])
-	local dy = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaY'])
-	local scroll = hs.eventtap.event.newScrollEvent({dx * scrollmult, dy * scrollmult},{},'pixel')
-	scroll:post()
-	
-	-- put the mouse back
-	hs.mouse.setAbsolutePosition(oldmousepos)
-	
-	return true, {scroll}
+		local dx = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaX'])
+		local dy = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaY'])
+		local scroll = hs.eventtap.event.newScrollEvent({dx * scrollmult, dy * scrollmult},{},'pixel')
+		scroll:post()
+
+		-- put the mouse back
+		hs.mouse.setAbsolutePosition(oldmousepos)
+
+		return true, {scroll}
     else
-	return false, {}
+		return false, {}
     end
 end)
 mousetap:start()
