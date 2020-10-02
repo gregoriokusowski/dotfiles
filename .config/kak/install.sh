e90bf87e1150736079fbd594a58ff8864d17786e
Gregorio Kusowski <gregorio.kusowski@gmail.com>
2020-10-02 23:44:20 +0200

    Revert "Removing kakoune config"
    This reverts commit da8c0ae3ae7c11c67c0d71db24272ade8f2928bb.

---
+++ .config/kak/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Installs plug.kak
+# https://github.com/andreyorst/plug.kak#installation
+mkdir -p ~/.config/kak/plugins/
+git clone https://github.com/andreyorst/plug.kak.git ~/.config/kak/plugins/plug.kak
