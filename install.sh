573f4eb2c36af6e49b6d9dc4a3444a6854280a1a
Gregorio Kusowski <gregorio.kusowski@shopify.com>
2022-01-23 11:33:17 +0100

    Some other configs

--- install.sh
+++ install.sh
@@ -13,11 +13,11 @@ echo "setting up nvim"
 sudo apt purge neovim
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt update
-sudo apt install -y neovim
+sudo apt install -y neovim fzf silversearcher-ag
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
 nvim --headless +PackerInstall +qa
 
-# echo "gitconfig" # this cuts out the user info
-# tail ~/dotfiles/.gitconfig --lines=+4 >> ~/.gitconfig
+echo "gitconfig" # this cuts out the user info
+tail ~/dotfiles/.gitconfig --lines=+4 >> ~/.gitconfig
